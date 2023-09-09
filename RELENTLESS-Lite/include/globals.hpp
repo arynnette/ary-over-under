f027da53a2f392d0510153b1bfc38791ca1c0fcb
ary <cjansah11@gmail.com>
2023-09-09 13:07:51 -0400

    do more stuff on scrim bot
    this is terrible, bad, terrible, and abominable code
    (it has odom implemented)

---
+++ RELENTLESS-Lite/include/globals.hpp
@@ -0,0 +1,24 @@
+#include "main.h"
+
+#define TRACK_WIDTH 11.5;
+
+namespace globals {
+    extern pros::Controller master;
+
+    extern pros::Motor motor_fl;
+    extern pros::Motor motor_ml;
+    extern pros::Motor motor_bl;
+    extern pros::Motor motor_fr;
+    extern pros::Motor motor_mr;
+    extern pros::Motor motor_br;
+
+    extern pros::Motor_Group left_drive;
+    extern pros::Motor_Group right_drive;
+
+    extern lemlib::Drivetrain_t chassis_odom;
+    extern Drive chassis;
+
+    extern pros::Rotation enc_left;
+    extern pros::Rotation enc_right;
+    extern pros::Rotation enc_theta;
+}
---
+++ RELENTLESS-Lite/src/globals.cpp
@@ -0,0 +1,43 @@
+#include "globals.hpp"
+
+using namespace ary;
+namespace globals {
+    // Chassis
+    pros::Controller master(CONTROLLER_MASTER);
+
+    pros::Motor motor_fl(4, MOTOR_GEARSET_6, true);
+    pros::Motor motor_ml(4, MOTOR_GEARSET_6, true);
+    pros::Motor motor_bl(4, MOTOR_GEARSET_6, true);
+    pros::Motor motor_fr(4, MOTOR_GEARSET_6, true);
+    pros::Motor motor_mr(4, MOTOR_GEARSET_6, true);
+    pros::Motor motor_br(4, MOTOR_GEARSET_6, true);
+
+    pros::Motor_Group left_drive({ motor_fl, motor_ml, motor_bl });
+    pros::Motor_Group right_drive({ motor_fr, motor_mr, motor_br });
+
+    lemlib::Drivetrain_t chassis_odom {
+        &left_drive,
+        &right_drive,
+        11.5,
+        2.75, 
+        450
+    };
+
+    Drive chassis(
+        {-4,-4,-4,-4},
+        {1,1,1,1},
+        16,
+        3.125,
+        600,
+        0.6
+    ); 
+
+    
+    // Electronics / Pneumatics / Sensors
+    // pros::Rotation enc_left();
+    // pros::Rotation enc_right();
+    // pros::Rotation enc_theta();
+
+    // Misc
+
+}
