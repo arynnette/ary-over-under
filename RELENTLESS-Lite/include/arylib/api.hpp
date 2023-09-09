a392a03bcd2b43961e017c27abcea28fe8bba88e
ary <cjansah11@gmail.com>
2023-09-09 12:16:07 -0400

    create scrim bot

---
+++ RELENTLESS-Lite/include/arylib/api.hpp
@@ -0,0 +1,14 @@
+/*
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#pragma once
+
+#include "arylib/PID.hpp"
+#include "arylib/auton.hpp"
+#include "arylib/auton_selector.hpp"
+#include "arylib/drive/drive.hpp"
+#include "arylib/autonselector.hpp"
+#include "arylib/util.hpp"
