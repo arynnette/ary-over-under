f911e0975a9ed1e7c8a7dbf956918c9bb5a94730
ary <cjansah11@gmail.com>
2023-09-30 15:55:35 -0400

    rewrite the autonomous

--- RELENTLESS/src/autons.cpp
+++ RELENTLESS/src/autons.cpp
@@ -8,6 +8,40 @@ using namespace globals;
 using namespace superstruct;
 
 void test_auton() {
-    
+  driveChassis(107, true);
+  chassis.wait_drive();
+
+  driveChassis(-38, true);
+  chassis.wait_drive();
+
+  turnChassis(-99);
+  chassis.wait_drive();
+
+  driveChassis(95, true);
+  chassis.wait_drive();
+
+  driveChassis(-10, true);
+  chassis.wait_drive();
+
+  turnChassis(15);
+  chassis.wait_drive();
+
+  driveChassis(40, true);
+  chassis.wait_drive();
+
+  leftSwing(60);
+  chassis.wait_drive();
+
+  driveChassis(-48, true);
+  chassis.wait_drive();
+
+  turnChassis(-90);
+  chassis.wait_drive();
+  
+  driveChassis(12, true);
+  chassis.wait_drive();
+
+  // chassis.set_drive_pid(2, DRIVE_SPEED, false, true);
+  // chassis.wait_drive();
 }
   //pros::task_t intakeTask(run_intake_for, (void*) malloc(sizeof(double)), TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "runIntakeFor");
