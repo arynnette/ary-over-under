7c3b0259698f7028cecd44a0f714953b9c9766d8
ary <cjansah11@gmail.com>
2023-11-10 02:22:20 -0500

    some notes

--- HVN-ON-EARTH/src/autons.cpp
+++ HVN-ON-EARTH/src/autons.cpp
@@ -18,6 +18,11 @@ using namespace superstruct;
   chassis.wait_until(double measurement) -> Waits for the drivetrain to reach a certain measurement before performing the next task.
 */
 
+/*
+  To run when our robot is starting on the far side from our alliance zone
+  /
+  close to our alliance goal.
+*/
 void near_side() {
   // Score the triball preload
   toggleIntake(true);
@@ -29,6 +34,7 @@ void near_side() {
   driveSync(22, true);
   driveSync(-19, true);
 
+  //Collect and score the back outer triball
   turnSync(-118);
   driveSync(50, true);
   pros::delay(150);
@@ -43,6 +49,8 @@ void near_side() {
   driveSync(35, true);
   driveSync(-35, true);
 
+
+  //Gain control of third triball, use wings to score last one
   turnSync(313);
   driveSync(40, true);
   pros::delay(400);
@@ -52,26 +60,35 @@ void near_side() {
   turnSync(90);
   toggleIntake(false);
   wings.open();
-  driveSync(55, true);
+  driveSync(65, true);
   wings.close();
   driveSync(-20, true);
-
-  //driveSync(100, true);
 }
 
+/*
+  To run when our robot is starting on the near side from our alliance zone
+  /
+  far from our alliance goal.
+*/
 void far_side() {
-  wings.open();
-  toggleIntake(true);
-  driveSync(30, true);
+  wings.open(); // Prepare for removal of match load triball
+  toggleIntake(true); // Secure preload triball
+
+  // Drive forward to removal match load triball with wings, close wings immediately
+  driveSync(30, true); 
   wings.close();
+
+  //Score the preload
   turnSync(40);
   toggleIntake(false);
   driveSync(35, true);
   driveSync(-10, true);
+
+  //Turn towards the elevation bar area
   turnSync(135);
   driveSync(90, true);
-  wings.open();
-  turnSync(120);
+  wings.open(); // Open our wings as they are being used to contact the bar
+  turnSync(120); // Turn slightly to guarantee contact
 }
 
 void skills() {
