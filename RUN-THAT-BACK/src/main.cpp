df004f6d82eb6b16d89e39e11d49e704242871cd
ary <cjansah11@gmail.com>
2023-11-20 08:09:37 -0500

    rah

--- RUN-THAT-BACK/src/main.cpp
+++ RUN-THAT-BACK/src/main.cpp
@@ -113,6 +113,7 @@ void competition_initialize() {}
 // get a path used for pure pursuit
 // this needs to be put outside a function
 ASSET(example_txt); // '.' replaced with "_" to make c++ happy
+ASSET(testingpath_txt);
 
 /**
  * Runs during auto
@@ -120,23 +121,20 @@ ASSET(example_txt); // '.' replaced with "_" to make c++ happy
  * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
 void autonomous() {
-    // example movement: Move to x: 20 and y:15, and face heading 90. Timeout set to 4000 ms
-    chassis.moveTo(20, 15, 90, 4000);
-    // example movement: Turn to face the point x:45, y:-45. Timeout set to 1000
-    // dont turn faster than 60 (out of a maximum of 127)
-    chassis.turnTo(45, -45, 1000, true, 60);
-    // example movement: Follow the path in path.txt. Lookahead at 15, Timeout set to 4000
-    // following the path with the back of the robot (forwards = false)
-    // see line 116 to see how to define a path
-    chassis.follow(example_txt, 15, 4000, false);
-    // wait until the chassis has travelled 10 inches. Otherwise the code directly after
-    // the movement will run immediately
-    // Unless its another movement, in which case it will wait
-    chassis.waitUntil(10);
-    pros::lcd::print(4, "Travelled 10 inches during pure pursuit!");
-    // wait until the movement is done
+    chassis.follow(testingpath_txt, 15, 4000);
     chassis.waitUntilDone();
-    pros::lcd::print(4, "pure pursuit finished!");
+    
+    // chassis.moveTo(20, 15, 90, 4000);
+    
+    // chassis.turnTo(45, -45, 1000, true, 60);
+    
+    // chassis.follow(example_txt, 15, 4000, false);
+    
+    // chassis.waitUntil(10);
+    // pros::lcd::print(4, "Travelled 10 inches during pure pursuit!");
+    
+    // chassis.waitUntilDone();
+    // pros::lcd::print(4, "pure pursuit finished!");
 }
 
 /**
