9ad8cf220e1ca9bcde5bf7c74ab4797227c8849e
ary <cjansah11@gmail.com>
2023-12-13 07:37:01 -0500

    remove old threading stuff
    move tasks to intake only, can be updated as needed

--- CLOUDS/src/main.cpp
+++ CLOUDS/src/main.cpp
@@ -5,20 +5,7 @@ using namespace superstruct;
 
 e_controlsch currentuser = RENU;
 
-void taskMethods() {
-	while (true) {
-		/*
-			Handle controls for whoever is driving the bot at the moment, each available user a respective set of controls
-		*/
-		renu_control();
- 		pros::delay(ary::util::DELAY_TIME);
-	}
-}
-
-pros::Task subsystemsTask(taskMethods);
-
 void initialize() {
-	subsystemsTask.suspend();
 	ary::printScr();
 	pros::delay(500); // Ports config
 
@@ -36,9 +23,9 @@ void initialize() {
 		Add autonomous paths to the auto selection
 	*/
 	ary::autonselector::auton_selector.add_autons({
+		Auton("Six triball\n\nThey ain't believe in us\nBut GOD DID :speaking_head:", six_triball),
 		Auton("Far side \n\nFar from alliance goal", near_side),
 		Auton("Near side (far from alliance goal) \n\nCLOSE TO ALLIANCE GOAL", far_side),
-		Auton("Six triball\n\nThey ain't believe in us\nBut GOD DID :speaking_head:", six_triball),
 		Auton("Skills \n\ncloudabunga", skills)
 	});
 
@@ -49,22 +36,21 @@ void initialize() {
 }
 
 void disabled() {
-	subsystemsTask.suspend();
 }
+
 void competition_initialize() {}
 
 void autonomous() {
-	subsystemsTask.resume();
 	autonomousResets();
 	ary::autonselector::auton_selector.call_selected_auton();
 }
 
 void opcontrol() {
-	subsystemsTask.resume();
 	disableActiveBrake();
 	opControlInit(); // Configure the chassis for driver control
 
 	while (true) {
 		globals::chassis.tank_control();
+		renu_control();	
 	}
 }
